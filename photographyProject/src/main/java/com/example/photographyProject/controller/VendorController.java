f35b624b0e64ed49ac43c055b09192f3cfbb04ac
devaditya yadav <adityayadav672@gmail.com>
2025-04-02 00:08:11 +0530

    update vendor controller
    vendor add api changed

--- photographyProject/src/main/java/com/example/photographyProject/controller/VendorController.java
+++ photographyProject/src/main/java/com/example/photographyProject/controller/VendorController.java
@@ -1,10 +1,13 @@
 package com.example.photographyProject.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.photographyProject.model.Vendor;
 import com.example.photographyProject.service.VendorDetailsService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/Vendors")
 public class VendorController {
@@ -24,10 +27,16 @@ public class VendorController {
 	private VendorDetailsService vendorService;
 	
 	@PostMapping("/add")
-	public ResponseEntity<String> addVendor(@RequestBody Vendor vendor){
-		vendorService.addVendor(vendor);
-		return new ResponseEntity<>("Vendor added successfully", HttpStatus.CREATED);
+	public ResponseEntity<Map<String, String>> addVendor(@RequestBody Vendor vendor){
+	    vendorService.addVendor(vendor);
+
+	    // Create a response map with the success message
+	    Map<String, String> response = new HashMap<>();
+	    response.put("message", "Vendor added successfully");
+
+	    return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
+
 	
 	@GetMapping("/list")
 	public ResponseEntity<List<Vendor>> getAllVendors(){
@@ -35,12 +44,12 @@ public class VendorController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Vendor> getVendorById(@PathVariable long id){
-		Vendor vendor = vendorService.getVendorById(id);
-        if (vendor != null) {
-            return new ResponseEntity<>(vendor, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	public ResponseEntity<Vendor> getVendorById(@PathVariable("id") Long id) { 
+	    Vendor vendor = vendorService.getVendorById(id);
+	    if (vendor != null) {
+	        return new ResponseEntity<>(vendor, HttpStatus.OK);
+	    }
+	    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 	
 	@DeleteMapping("/{id}")
